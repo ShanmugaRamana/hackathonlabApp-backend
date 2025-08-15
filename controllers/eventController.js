df4d0e1ec7199777908c6badff33e88c4e22a29d
ShanmugaRamana <shanmugaramana27@gmail.com>
2025-08-15 16:06:10 +0530

    updated event routes

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -119,4 +119,16 @@ const getDashboardEvents = async (req, res) => {
   }
 };
 
-module.exports = { createEvent, getEvents, getDashboardEvents };
+const getEventById = async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.id);
+    if (event) {
+      res.json(event);
+    } else {
+      res.status(404).json({ message: 'Event not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+module.exports = { createEvent, getEvents, getDashboardEvents, getEventById };
--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getEvents } = require('../controllers/eventController');
+const { getEvents, getEventById } = require('../controllers/eventController');
 const { protect } = require('../middleware/authMiddleware');
 
 // This route is for the mobile app to fetch events
 // GET /api/events
 router.get('/', protect, getEvents);
+router.get('/:id', protect, getEventById);
 
 module.exports = router;
