2b21513db78bc5b1c0423db142b769e6632cbd77
Shanmuga Ramana <158463758+ShanmugaRamana@users.noreply.github.com>
2025-09-24 13:47:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -170,7 +170,6 @@ app.use((error, req, res, next) => {
   
   res.status(500).json({ message: 'Internal server error' });
 });
-
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
