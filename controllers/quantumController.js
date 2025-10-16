ffb727cc89a9ccaefda65bb13b6d3dddb0324440
ShanmugaRamana <shanmugaramana27@gmail.com>
2025-10-16 15:12:04 +0530

    updated quantum AI feature launch time

--- controllers/quantumController.js
+++ controllers/quantumController.js
@@ -5,7 +5,7 @@ const getLaunchDate = (req, res) => {
   // --- THIS IS THE FIX ---
   // A single, constant launch date that is the same for every user, every time.
   // We'll set it to October 21, 2025, at 11:30 PM India Standard Time.
-  const fixedLaunchDate = new Date('2025-10-21T23:30:00.000+05:30');
+  const fixedLaunchDate = new Date('2025-11-21T23:30:00.000+05:30');
 
   res.status(200).json({
     launchDate: fixedLaunchDate.toISOString(),
